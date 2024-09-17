3f268cf02c76203836fa90174d972d6a39082dea
stamplevskiyd <stamplevskiyd@gmail.com>
2024-09-17 14:27:27 +0300

    Updated connector base, added Leads stream

--- airbyte-integrations/connectors/source-amocrm/source_amocrm/source.py
+++ airbyte-integrations/connectors/source-amocrm/source_amocrm/source.py
@@ -4,13 +4,14 @@
 
 
 from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple, Literal
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import pendulum
 import requests
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_protocol.models import SyncMode
+
 from .auth import CredentialsCraftAuthenticator, AmoCrmAuthenticator
 
 
@@ -19,34 +20,17 @@ class AmoCrmStream(HttpStream, ABC):
     def __init__(self, config: Mapping[str, Any], authenticator: CredentialsCraftAuthenticator | AmoCrmAuthenticator):
         super().__init__(authenticator=None)
         self._authenticator = authenticator
-        self.subdomain = config["subdomain"]
+        self._subdomain = config["subdomain"]
+        self._limit: int = 250  # default for most AmoCrm urls
+        self._query: int | str | None = None
+        self._time_from: pendulum.DateTime = config["time_from_transformed"]
+        self._time_to: pendulum.DateTime = config["time_to_transformed"]
+        self._with: str = ""
+        self._custom_filters: list[str] = []
 
     @property
     def url_base(self) -> str:
-        return f"https://{self.subdomain}.amocrm.ru/api/"
-
-
-class Contacts(AmoCrmStream):
-    primary_key = "id"
-
-    def __init__(self, config: Mapping[str, Any], authenticator: CredentialsCraftAuthenticator | AmoCrmAuthenticator = None):
-        super().__init__(config, authenticator)
-        self._with: str = config.get("with")
-        self._limit: int = 250
-        self._query: int | str | None = config.get("query")
-        self._filter: dict[str, any] | None = None
-        self._order_direction: Literal["asc", "desc"] = "asc"
-        self._order_field: Literal["updated_at", "id"] = "id"
-
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        return "v4/contacts"
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        if len(response.json()["_embedded"]["contacts"]) < self._limit:
-            return None
-        return {"page_number": response.json()["_page"] + 1}
+        return f"https://{self._subdomain}.amocrm.ru/api/"
 
     def request_headers(
         self,
@@ -67,20 +51,71 @@ class Contacts(AmoCrmStream):
         params: dict[str, any] = {
             "page": page,
             "limit": self._limit,
+            "order[id]": "asc",
         }
         if self._with:
             params["with"] = self._with
         if self._query:
             params["query"] = self._query
-        if self._filter:
-            params["filter"] = self._filter
-        if self._order_field:
-            params[f"order[{self._order_field}]"] = self._order_direction
+
+        # Add custom filters
+        if self._custom_filters:
+            for custom_filter in self._custom_filters:
+                field, value = custom_filter.split("=")
+                params[field] = value
+
+        # Add created date filter
+        params["filter[created_at][from]"] = self._time_from.timestamp()
+        params["filter[created_at][to]"] = self._time_to.timestamp()
 
         return params
 
+
+class Contacts(AmoCrmStream):
+    primary_key = "id"
+
+    def __init__(self, config: Mapping[str, Any], authenticator: CredentialsCraftAuthenticator | AmoCrmAuthenticator = None):
+        super().__init__(config, authenticator)
+        self._custom_filters = config.get("contacts_filters", [])
+        # Add all possible with values to load full data
+        self._with = ",".join(["catalog_elements", "leads", "customers"])
+        self._query = config.get("contacts_query", None)
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "v4/contacts"
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        if len(response.json()["_embedded"]["contacts"]) < self._limit:
+            return None
+        return {"page_number": response.json()["_page"] + 1}
+
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         yield from response.json()["_embedded"]["contacts"]
+
+
+class Leads(AmoCrmStream):
+    primary_key = "id"
+
+    def __init__(self, config: Mapping[str, Any], authenticator: CredentialsCraftAuthenticator | AmoCrmAuthenticator = None):
+        super().__init__(config, authenticator)
+        self._custom_filters = config.get("leads_filters", [])
+        self._with = ",".join(["catalog_elements", "is_price_modified_by_robot", "loss_reason", "contacts", "source_id"])
+        self._query = config.get("leads_query", None)
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "v4/leads"
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        if len(response.json()["_embedded"]["leads"]) < self._limit:
+            return None
+        return {"page_number": response.json()["_page"] + 1}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        yield from response.json()["_embedded"]["leads"]
 
 
 class SourceAmoCrm(AbstractSource):
@@ -106,7 +141,7 @@ class SourceAmoCrm(AbstractSource):
             else:
                 time_to = today_date.subtract(days=1)
         elif date_range_type == "last_n_days":
-            time_from = today_date.subtract(days=date_range.get("last_days_count"))
+            time_from = today_date.subtract(days=date_range.get("last_days"))
             if date_range.get("should_load_today"):
                 time_to = today_date
             else:
@@ -135,10 +170,19 @@ class SourceAmoCrm(AbstractSource):
             raise Exception("Invalid Auth type. Available: access_token_auth and credentials_craft_auth")
 
     def check_connection(self, logger, config) -> Tuple[bool, any]:
+        streams = self.streams(config)
+        for stream in streams:
+            stream._limit = 1  # Decrease amount of loaded values to speed up check
+            try:
+                next(stream.read_records(sync_mode=SyncMode.full_refresh))
+            except Exception as ex:
+                return False, ex
+
         return True, None
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+    def streams(self, config: Mapping[str, Any]) -> List[AmoCrmStream]:
         config = self.transform_config(config)
         auth = self.get_auth(config)
         contacts_stream = Contacts(config, auth)
-        return [contacts_stream]
+        leads_stream = Leads(config, auth)
+        return [contacts_stream, leads_stream]

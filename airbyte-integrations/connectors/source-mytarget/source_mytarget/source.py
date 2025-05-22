ea735e0e3456723cee4e1909e6e44bab08579570
Denis <dehgsk58>
2025-05-23 02:07:04 +0900

    Ruff format Mytarget

--- airbyte-integrations/connectors/source-mytarget/source_mytarget/source.py
+++ airbyte-integrations/connectors/source-mytarget/source_mytarget/source.py
@@ -27,9 +27,16 @@ from .auth import CredentialsCraftAuthenticator
 
 # Source
 class SourceMytarget(AbstractSource):
-
-    obj_streams_classes: List[ObjectStream] = [Campaigns, Banners, PadsTrees, PackagesPads]
-    stat_streams_classes: List[IncrementalStatisticsMixin] = [CampaignsStatistics, BannersStatistics]
+    obj_streams_classes: List[ObjectStream] = [
+        Campaigns,
+        Banners,
+        PadsTrees,
+        PackagesPads,
+    ]
+    stat_streams_classes: List[IncrementalStatisticsMixin] = [
+        CampaignsStatistics,
+        BannersStatistics,
+    ]
 
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         config = self.prepare_config(config)
@@ -41,26 +48,38 @@ class SourceMytarget(AbstractSource):
                 return auth_conn_check
 
         prepared_date_range: Dict[str, Any] = config["prepared_date_range"]
-        if not prepared_date_range.get("date_from") or not prepared_date_range.get("date_to"):
+        if (
+            not prepared_date_range.get("date_from")
+            or not prepared_date_range.get("date_to")
+        ):
             return False, "You must specify date_range in connector specification."
 
         for obj_stream_class in self.obj_streams_classes:
             obj_stream = obj_stream_class(authenticator=auth, config=config)
             if obj_stream.use_included_fields:
-                obj_stream.fields_to_include = config.get(obj_stream.included_fields_property_name(), [])
+                obj_stream.fields_to_include = config.get(
+                    obj_stream.included_fields_property_name(), []
+                )
             else:
                 obj_stream.fields_to_include = []
             obj_stream.page_size = 1
 
             if obj_stream.primary_key not in obj_stream.fields:
-                return False, f"Fields for stream {obj_stream.name} must contain '{obj_stream.primary_key}' field."
+                return (
+                    False,
+                    f"Fields for stream {obj_stream.name} must contain '{obj_stream.primary_key}' field.",
+                )
 
             test_response_data = requests.get(
-                obj_stream.url_base + obj_stream.path(), headers=auth.get_auth_header(), params=obj_stream.request_params()
+                obj_stream.url_base + obj_stream.path(),
+                headers=auth.get_auth_header(),
+                params=obj_stream.request_params(),
             ).json()
 
             if test_response_data.get("error"):
-                return False, test_response_data["error"].get("message", str(test_response_data))
+                return False, test_response_data["error"].get(
+                    "message", str(test_response_data)
+                )
 
         return True, None
 
@@ -70,11 +89,17 @@ class SourceMytarget(AbstractSource):
         elif config["credentials"]["auth_type"] == "credentials_craft_auth":
             return CredentialsCraftAuthenticator(
                 credentials_craft_host=config["credentials"]["credentials_craft_host"],
-                credentials_craft_token=config["credentials"]["credentials_craft_token"],
-                credentials_craft_token_id=config["credentials"]["credentials_craft_token_id"],
+                credentials_craft_token=config["credentials"][
+                    "credentials_craft_token"
+                ],
+                credentials_craft_token_id=config["credentials"][
+                    "credentials_craft_token_id"
+                ],
             )
         else:
-            raise Exception("Invalid Auth type. Available: access_token_auth and credentials_craft_auth")
+            raise Exception(
+                "Invalid Auth type. Available: access_token_auth and credentials_craft_auth"
+            )
 
     @staticmethod
     def prepare_config_datetime(config: Mapping[str, Any]) -> Mapping[str, Any]:
@@ -94,7 +119,9 @@ class SourceMytarget(AbstractSource):
             else:
                 prepared_range["date_to"] = today - timedelta(days=1)
         elif range_type == "last_n_days":
-            prepared_range["date_from"] = today - timedelta(days=date_range["last_days_count"])
+            prepared_range["date_from"] = today - timedelta(
+                days=date_range["last_days_count"]
+            )
             if date_range.get("should_load_today", False):
                 prepared_range["date_to"] = today
             else:
@@ -103,10 +130,14 @@ class SourceMytarget(AbstractSource):
             raise ValueError("Invalid date_range_type")
 
         if isinstance(prepared_range["date_from"], datetime):
-            prepared_range["date_from"] = prepared_range["date_from"].strftime(config_date_format)
+            prepared_range["date_from"] = prepared_range["date_from"].strftime(
+                config_date_format
+            )
 
         if isinstance(prepared_range["date_to"], datetime):
-            prepared_range["date_to"] = prepared_range["date_to"].strftime(config_date_format)
+            prepared_range["date_to"] = prepared_range["date_to"].strftime(
+                config_date_format
+            )
 
         config["prepared_date_range"] = prepared_range
         return config
@@ -140,8 +171,17 @@ class SourceMytarget(AbstractSource):
     def prepare_config(self, config: Mapping[str, Any]) -> Mapping[str, Any]:
         config = self.prepare_config_datetime(config)
         for obj_stream in [s(config={}) for s in self.obj_streams_classes]:
-            splitted_included_fields = list(filter(None, config.get(obj_stream.included_fields_property_name(), "").split(",")))
-            config.update({obj_stream.included_fields_property_name(): splitted_included_fields})
+            splitted_included_fields = list(
+                filter(
+                    None,
+                    config.get(obj_stream.included_fields_property_name(), "").split(
+                        ","
+                    ),
+                )
+            )
+            config.update(
+                {obj_stream.included_fields_property_name(): splitted_included_fields}
+            )
 
         return config
 
@@ -150,7 +190,13 @@ class SourceMytarget(AbstractSource):
         auth = self.get_auth(config)
         return [
             *[
-                obj_stream(authenticator=auth, config=config, fields_to_include=config.get(obj_stream.included_fields_property_name(), []))
+                obj_stream(
+                    authenticator=auth,
+                    config=config,
+                    fields_to_include=config.get(
+                        obj_stream.included_fields_property_name(), []
+                    ),
+                )
                 for obj_stream in self.obj_streams_classes
             ],
             *[
--- airbyte-integrations/connectors/source-mytarget/source_mytarget/streams.py
+++ airbyte-integrations/connectors/source-mytarget/source_mytarget/streams.py
@@ -3,7 +3,11 @@ from datetime import datetime, timedelta
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Dict
 
 import requests
-from airbyte_cdk.sources.streams.core import IncrementalMixin, SyncMode, package_name_from_class
+from airbyte_cdk.sources.streams.core import (
+    IncrementalMixin,
+    SyncMode,
+    package_name_from_class,
+)
 from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
 from airbyte_cdk.sources.streams.http.auth import HttpAuthenticator
 from airbyte_cdk.sources.utils.schema_helpers import ResourceSchemaLoader
@@ -12,9 +16,12 @@ from airbyte_cdk.sources.utils.transform import TransformConfig, TypeTransformer
 from .utils import DATE_FORMAT, chunks
 import json
 
+
 # Basic full refresh stream
 class MyTargetStream(HttpStream, ABC):
-    transformer: TypeTransformer = TypeTransformer(config=TransformConfig.DefaultSchemaNormalization)
+    transformer: TypeTransformer = TypeTransformer(
+        config=TransformConfig.DefaultSchemaNormalization
+    )
 
     url_base = "https://target.my.com/"
     primary_key = "id"
@@ -24,7 +31,9 @@ class MyTargetStream(HttpStream, ABC):
         super().__init__(authenticator)
         self.config = config
 
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+    def next_page_token(
+        self, response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
         data = response.json()
         if not data.get("items"):
             return None
@@ -50,10 +59,17 @@ class MyTargetStream(HttpStream, ABC):
 
         return schema
 
-    def request_params(self, next_page_token: Mapping[str, Any] = None, **kwargs) -> MutableMapping[str, Any]:
-        return {"limit": self.page_size, "offset": next_page_token["offset"] if next_page_token else 0}
+    def request_params(
+        self, next_page_token: Mapping[str, Any] = None, **kwargs
+    ) -> MutableMapping[str, Any]:
+        return {
+            "limit": self.page_size,
+            "offset": next_page_token["offset"] if next_page_token else 0,
+        }
 
-    def parse_response(self, response: requests.Response, *args, **kwargs) -> Iterable[Mapping]:
+    def parse_response(
+        self, response: requests.Response, *args, **kwargs
+    ) -> Iterable[Mapping]:
         yield from map(self.add_constants_to_record, response.json()["items"])
 
 
@@ -62,7 +78,12 @@ class ObjectStream(MyTargetStream, ABC):
     object_name_plural = None
     use_included_fields = True
 
-    def __init__(self, config: Mapping[str, Any], authenticator: HttpAuthenticator = None, fields_to_include: Iterable[str] = []):
+    def __init__(
+        self,
+        config: Mapping[str, Any],
+        authenticator: HttpAuthenticator = None,
+        fields_to_include: Iterable[str] = [],
+    ):
         super().__init__(authenticator=authenticator, config=config)
         self.fields_to_include = fields_to_include
 
@@ -73,7 +94,9 @@ class ObjectStream(MyTargetStream, ABC):
         object_schema = super().get_json_schema()
         if self.fields_to_include:
             if not self.use_included_fields:
-                raise Exception(f"Included fields is not supported for {self.name} stream.")
+                raise Exception(
+                    f"Included fields is not supported for {self.name} stream."
+                )
             included_fields_schemas = {}
             object_schema_properties_keys = object_schema["properties"].keys()
             for field_to_include_name in self.fields_to_include:
@@ -82,7 +105,9 @@ class ObjectStream(MyTargetStream, ABC):
                         f"Field '{field_to_include_name}' isn't available for {self.name} stream. "
                         f"Available fields: {object_schema_properties_keys}"
                     )
-                included_fields_schemas[field_to_include_name] = object_schema["properties"][field_to_include_name]
+                included_fields_schemas[field_to_include_name] = (
+                    object_schema["properties"][field_to_include_name]
+                )
             object_schema["properties"] = included_fields_schemas
             extra_properties = ["__productName", "__clientName"]
             custom_keys = json.loads(self.config.get("custom_json", "{}")).keys()
@@ -102,7 +127,9 @@ class ObjectStream(MyTargetStream, ABC):
     def included_fields_property_name(cls):
         return cls.object_name_plural + "_included_fields"
 
-    def request_params(self, next_page_token: Mapping[str, Any] = None, **kwargs) -> MutableMapping[str, Any]:
+    def request_params(
+        self, next_page_token: Mapping[str, Any] = None, **kwargs
+    ) -> MutableMapping[str, Any]:
         params = super().request_params(next_page_token, **kwargs)
         params.update({"fields": ",".join(self.fields)})
         return params
@@ -119,7 +146,7 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
         authenticator: HttpAuthenticator,
         date_from: str,
         date_to: str,
-        config: Mapping[str, Any]
+        config: Mapping[str, Any],
     ):
         MyTargetStream.__init__(self, config, authenticator)
         HttpSubStream.__init__(self, self.parent_stream_class(config, authenticator))
@@ -132,16 +159,26 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
     def path(self, *args, **kwargs) -> str:
         return f"/api/v2/statistics/{self.parent.object_name_plural}/day.json"
 
-    def request_params(self, stream_slice: Mapping[str, Any] = None, *args, **kwargs) -> MutableMapping[str, Any]:
-        params = {"date_from": stream_slice["date_from"], "date_to": stream_slice["date_to"], "id": ",".join(stream_slice["ids"])}
+    def request_params(
+        self, stream_slice: Mapping[str, Any] = None, *args, **kwargs
+    ) -> MutableMapping[str, Any]:
+        params = {
+            "date_from": stream_slice["date_from"],
+            "date_to": stream_slice["date_to"],
+            "id": ",".join(stream_slice["ids"]),
+        }
         return params
 
     def next_page_token(self, *args) -> Optional[Mapping[str, Any]]:
         return None
 
     def get_json_schema(self) -> Mapping[str, Any]:
-        schema = ResourceSchemaLoader(package_name_from_class(self.__class__)).get_schema("statistics")
-        schema["properties"].update({self.object_id_property_name: {"type": ["null", "string"]}})
+        schema = ResourceSchemaLoader(
+            package_name_from_class(self.__class__)
+        ).get_schema("statistics")
+        schema["properties"].update(
+            {self.object_id_property_name: {"type": ["null", "string"]}}
+        )
         extra_properties = ["__productName", "__clientName"]
         custom_keys = json.loads(self.config.get("custom_json", "{}")).keys()
         extra_properties.extend(custom_keys)
@@ -150,7 +187,9 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
 
         return schema
 
-    def parse_response(self, response: requests.Response, *args, **kwargs) -> Iterable[Mapping]:
+    def parse_response(
+        self, response: requests.Response, *args, **kwargs
+    ) -> Iterable[Mapping]:
         for obj in MyTargetStream.parse_response(self, response, *args, **kwargs):
             obj_id = obj.get("id")
             if not obj_id:
@@ -180,17 +219,29 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
         self._cursor_value = value.get(self.cursor_field, None)
 
     def read_records(
-        self, sync_mode, cursor_field: List[str] = None, stream_slice: Mapping[str, Any] = None, stream_state: Mapping[str, Any] = None
+        self,
+        sync_mode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
     ) -> Iterable[Mapping[str, Any]]:
-        for record in MyTargetStream.read_records(self, sync_mode, cursor_field, stream_slice, stream_state):
+        for record in MyTargetStream.read_records(
+            self, sync_mode, cursor_field, stream_slice, stream_state
+        ):
             # Update cursor value only when all obj IDs chunks processed.
             if stream_slice["should_update_state_after"]:
                 latest_record_date = record[self.cursor_field]
-                self._cursor_value = max(self._cursor_value, latest_record_date) if self._cursor_value else latest_record_date
+                self._cursor_value = (
+                    max(self._cursor_value, latest_record_date)
+                    if self._cursor_value
+                    else latest_record_date
+                )
             yield record
 
     @staticmethod
-    def date_chunks_ranges(date_from: str, skip_first: bool, date_to: str = None) -> List[Dict[str, Any]]: #List[str]:
+    def date_chunks_ranges(
+        date_from: str, skip_first: bool, date_to: str = None
+    ) -> List[Dict[str, Any]]:  # List[str]:
         """
         Returns a list of each day between the date_from and yesterday.
         The return value is a list of dicts {'date': date_string}.
@@ -217,7 +268,11 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
         return chunks_ranges
 
     def stream_slices(
-        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+        self,
+        *,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_state: Mapping[str, Any] = None,
     ) -> Iterable[Optional[Mapping[str, Any]]]:
         """
         stream_state always describing already loaded records state, and also we shouldn't load
@@ -234,14 +289,21 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
             # if not first load (stream_state has been properly passed to stream):
             if stream_state and self.cursor_field in stream_state:
                 # if state is abnormal (state date is more than yesterday), we force it to yesterday
-                if datetime.strptime(stream_state[self.cursor_field], DATE_FORMAT).date() > datetime.today().date() - timedelta(1):
-                    stream_state[self.cursor_field] = (datetime.today().date() - timedelta(1)).strftime(DATE_FORMAT)
+                if (
+                    datetime.strptime(stream_state[self.cursor_field], DATE_FORMAT).date()
+                    > datetime.today().date() - timedelta(1)
+                ):
+                    stream_state[self.cursor_field] = (
+                        (datetime.today().date() - timedelta(1)).strftime(DATE_FORMAT)
+                    )
                 else:
                     date_from = stream_state[self.cursor_field]
             # if first load:
             else:
                 date_from = self.date_from
-        date_ranges = self.date_chunks_ranges(date_from, skip_first=bool(stream_state), date_to=self.date_to)
+        date_ranges = self.date_chunks_ranges(
+            date_from, skip_first=bool(stream_state), date_to=self.date_to
+        )
         if not date_ranges:
             # Skip for unnecessary objects IDs retrieving - there will be no any slices if we have no dates to load
             self.logger.info(f"{self.name} stream doesn't need refreshing.")
@@ -249,17 +311,30 @@ class IncrementalStatisticsMixin(MyTargetStream, IncrementalMixin, HttpSubStream
 
         # chunked because of 200 object ids per request restriction
         objects_ids_chunks = list(
-            chunks([str(r["parent"]["id"]) for r in list(HttpSubStream.stream_slices(self, sync_mode, cursor_field, stream_state))], 200)
+            chunks(
+                [
+                    str(r["parent"]["id"])
+                    for r in list(
+                        HttpSubStream.stream_slices(
+                            self, sync_mode, cursor_field, stream_state
+                        )
+                    )
+                ],
+                200,
+            )
         )
         slices = []
         for date_range in date_ranges:
-            for ids_chunk_num, objects_ids_chunk in enumerate(objects_ids_chunks, start=1):
+            for ids_chunk_num, objects_ids_chunk in enumerate(
+                objects_ids_chunks, start=1
+            ):
                 slices.append(
                     {
                         "ids": objects_ids_chunk,
                         "date_from": date_range["date_from"],
                         "date_to": date_range["date_to"],
-                        "should_update_state_after": ids_chunk_num == len(objects_ids_chunks),
+                        "should_update_state_after": ids_chunk_num
+                        == len(objects_ids_chunks),
                     }
                 )
         return slices

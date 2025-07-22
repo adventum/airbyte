d293eed31c7d5087625fb3fec42c3b266b6b4c8e
Denis <dehgsk58>
2025-07-22 16:25:38 +0900

    Corrected the parsing of the date pattern, now all files matching the specified date range are loaded

--- airbyte-integrations/connectors/source-yandex-disk/source_yandex_disk/auth.py
+++ airbyte-integrations/connectors/source-yandex-disk/source_yandex_disk/auth.py
@@ -5,7 +5,12 @@ import requests
 
 
 class CredentialsCraftAuthenticator(TokenAuthenticator):
-    def __init__(self, credentials_craft_host: str, credentials_craft_token: str, credentials_craft_token_id: int):
+    def __init__(
+        self,
+        credentials_craft_host: str,
+        credentials_craft_token: str,
+        credentials_craft_token_id: int,
+    ):
         self._cc_host = credentials_craft_host
         self._cc_token = credentials_craft_token
         self._cc_token_id = credentials_craft_token_id
@@ -16,23 +21,36 @@ class CredentialsCraftAuthenticator(TokenAuthenticator):
 
     @property
     def _service_access_token(self) -> Mapping[str, Any]:
-        resp = requests.get(self._url, headers={"Authorization": f"Bearer {self._cc_token}"}).json()
+        resp = requests.get(
+            self._url, headers={"Authorization": f"Bearer {self._cc_token}"}
+        ).json()
         return resp.get("access_token")
 
     def get_auth_header(self) -> Mapping[str, Any]:
-        super().__init__(token=self._service_access_token, auth_method="OAuth", auth_header="Authorization")
+        super().__init__(
+            token=self._service_access_token,
+            auth_method="OAuth",
+            auth_header="Authorization",
+        )
         return super().get_auth_header()
 
     def check_connection(self):
         try:
             requests.get(self._cc_host)
-        except:
+        except requests.exceptions.RequestException:
             return False, f"Connection to {self._cc_host} timed out"
-        resp_for_except = requests.get(self._url, headers={"Authorization": f"Bearer {self._cc_token}"})
+        resp_for_except = requests.get(
+            self._url, headers={"Authorization": f"Bearer {self._cc_token}"}
+        )
         try:
-            token_resp = requests.get(self._url, headers={"Authorization": f"Bearer {self._cc_token}"}).json()
-        except:
-            return {"error_code": resp_for_except.status_code, "text_response": resp_for_except.text}
+            token_resp = requests.get(
+                self._url, headers={"Authorization": f"Bearer {self._cc_token}"}
+            ).json()
+        except requests.exceptions.RequestException:
+            return {
+                "error_code": resp_for_except.status_code,
+                "text_response": resp_for_except.text,
+            }
         if token_resp.get("error"):
             return False, f"CredentialsCraft error: {token_resp.get('error')}"
 

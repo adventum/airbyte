143914b0729b11f71bcd1a58035fbdc2fb75a58d
stamplevskiyd <stamplevskiyd@gmail.com>
2025-08-08 15:43:59 +0300

    Added salebot source connector

---
+++ airbyte-integrations/connectors/source-salebot/source_salebot/streams/base_stream.py
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+from abc import ABC, abstractmethod
+from typing import Any, Mapping, MutableMapping, Optional
+
+import pendulum
+import requests
+from airbyte_cdk.sources.streams.http import HttpStream
+
+
+class SalebotStream(HttpStream, ABC):
+    url_base = "https://chatter.salebot.pro/api/"
+
+    def __init__(
+        self,
+        api_key: str,
+        time_from: pendulum.DateTime | None = None,
+        time_to: pendulum.DateTime | None = None,
+    ) -> None:
+        super().__init__(authenticator=None)
+        self._api_key = api_key
+        self._time_from = time_from
+        self._time_to = time_to
+
+    def next_page_token(
+        self, response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> MutableMapping[str, Any]:
+        return {}
+
+    @abstractmethod
+    def request_body_json(
+        self,
+        stream_state: Optional[Mapping[str, Any]],
+        stream_slice: Optional[Mapping[str, Any]] = None,
+        next_page_token: Optional[Mapping[str, Any]] = None,
+    ) -> Optional[Mapping[str, Any]]: ...
+
+    @property
+    def state(self) -> MutableMapping[str, Any]:
+        """Get connector state (default from HttpStream)"""
+        cursor = self.get_cursor()
+        if cursor:
+            return cursor.get_stream_state()  # type: ignore
+        return self._state
+
+    @state.setter
+    def state(self, value: MutableMapping[str, Any]) -> None:
+        """Set empty state as in old connectors"""
+        value = {}
+        cursor = self.get_cursor()
+        if cursor:
+            cursor.set_initial_state(value)
+        self._state = value
